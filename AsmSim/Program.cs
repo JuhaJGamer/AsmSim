5f4a9cd78d8e7c8c1b45653949e1518f946396aa
JuhaJGamer <juhajgamer@live.fi>
2017-07-27 16:15:05 +0300

    Add project files.

---
+++ AsmSim/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsmSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
